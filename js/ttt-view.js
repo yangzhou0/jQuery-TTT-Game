8a42786be292240cc7bd866ddcc1638e85392e99
Yang Zhou <yzhou3991@gmail.com>
2018-05-13 01:23:33 -0700

    start writing setupBoard method: make 3x3 squres

--- js/ttt-view.js
+++ js/ttt-view.js
@@ -10,7 +10,14 @@ class View {
   makeMove($square) {}
 
   setupBoard() {
-    
+    let $grid = $('<ul>');
+    for (let i = 0; i < 3; i ++){
+      for(let j = 0; j < 3; j ++){
+        let $square = $('<li>').attr('pos',[i,j]);
+        
+      }
+
+    }
   }
 }
 
