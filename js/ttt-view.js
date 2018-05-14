f8edc6afeeaca313fd115c68d683fa87f77a244c
Yang Zhou <yzhou3991@gmail.com>
2018-05-14 05:53:39 -0400

    write click event for each cell

--- js/ttt-view.js
+++ js/ttt-view.js
@@ -1,22 +1,30 @@
 class View {
   constructor(game, $el) {
-    this.game= game;
+    this.game = game;
     this.$el = $el;
     this.setupBoard();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    $('.ttt').on("click",'.square',function(e) {
+      let $square = $(e.currentTarget);
+      $square.css("background-color", "white");
+
+    });
+  }
 
   makeMove($square) {}
 
   setupBoard() {
-    let $board = $('<ul>');
-    for (let i = 0; i < 3; i ++){
-      for(let j = 0; j < 3; j ++){
-        let $square = $('<li>').addClass('square').attr('pos',[i,j]);
-        $board.append($square);
+    let $board = $("<ul>");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let $square = $("<li>")
+          .addClass("square")
+          .attr("pos", [i, j]);
+          $board.append($square);
+          this.bindEvents();
       }
-
     }
     this.$el.append($board);
   }
