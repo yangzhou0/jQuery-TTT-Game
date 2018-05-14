be2eb6d806300a71ba108b42e9ab5b99e2dadf3d
Yang Zhou <yzhou3991@gmail.com>
2018-05-14 02:19:03 -0400

    build setUpBoard method

--- js/ttt-view.js
+++ js/ttt-view.js
@@ -10,14 +10,15 @@ class View {
   makeMove($square) {}
 
   setupBoard() {
-    let $grid = $('<ul>');
+    let $board = $('<ul>');
     for (let i = 0; i < 3; i ++){
       for(let j = 0; j < 3; j ++){
-        let $square = $('<li>').attr('pos',[i,j]);
-        
+        let $square = $('<li>').addClass('square').attr('pos',[i,j]);
+        $board.append($square);
       }
 
     }
+    this.$el.append($board);
   }
 }
 
